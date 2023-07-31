3878c0c7f8a51f5c302dc8dd52c851af6645cd29
Yuuki175 <realouise175@gmail.com>
2023-07-31 15:49:35 +0800

    4th week

--- Dist/SCSS/Script.js
+++ Dist/SCSS/Script.js
@@ -26,24 +26,42 @@ window.addEventListener("scroll", () => {
 const modal = document.querySelector('.modal');
 const btnRegion = document.querySelector('.region');
 const btnClose = document.querySelector('#closeModal');
+const overlay = document.querySelector('#overlay');
 
 const openModalHandler = () => {
  btnRegion.classList.toggle('active');
- btnRegion.nextElementSibling.classList.toggle('active');
  modal.classList.toggle('active');
+ overlay.classList.toggle('active');
 }
 
 const closeModalHandler = () => {
  modal.classList.toggle('active');
  btnRegion.classList.toggle('active');
+ overlay.classList.toggle('active');
 }
 
-const closeModalOnFocusOut = (event) => {
- if (!modal.contains(event.target)) {
-  closeModalHandler();
- }
+const closeOverlay = () => {
+modal.classList.toggle('active');
+btnRegion.classList.toggle('active');
+overlay.classList.toggle('active');
 }
 
+var slider = tns({
+    container:'.slider',
+    items: 1,
+    slideBy:'page',
+    autoplay: true,
+    mouseDrag: true,
+    navPosition: "bottom",
+    responsive: {
+        900: {
+            items: 4
+        }
+    }
+})
+
+overlay.addEventListener('click', closeOverlay);
 btnClose.addEventListener('click', closeModalHandler);
 btnRegion.addEventListener('click', openModalHandler);
-window.addEventListener('click', closeModalOnFocusOut);
+
+
